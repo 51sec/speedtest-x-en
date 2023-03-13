8897940afda55ab6366f8b8ad45619fee491365a
51sec <johnyan.ca@gmail.com>
2023-03-13 12:44:12 -0400

    Update config.php

--- backend/config.php
+++ backend/config.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * 最多保存多少条测试记录
+ * How many test records will be kept?
  */
-const MAX_LOG_COUNT = 100;
+const MAX_LOG_COUNT = 500;
 
 /**
  * IP运营商解析服务：(1) ip.sb | (2) ipinfo.io （如果1解析ip异常，请切换成2）
@@ -11,6 +11,6 @@ const MAX_LOG_COUNT = 100;
 const IP_SERVICE = 'ip.sb';
 
 /**
- * 是否允许同一IP记录多条测速结果
+ * If allow record same ip's results again?
  */
-const SAME_IP_MULTI_LOGS = false;
+const SAME_IP_MULTI_LOGS = true;
